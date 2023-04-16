59835bced8cc10c4910ae11edcbe448add90b7bc
vlad <sp0ngbik@gmail.com>
2023-04-16 12:24:36 +0300

    start project first promo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import style from "./style.module.scss";
 import "./App.css";
 
 function App() {
@@ -7,26 +8,46 @@ function App() {
   const setTodoFunc = (todoItem) => {
     setTodoList([todoItem, ...todoList]);
   };
+  const funcDeleteTodo = (todoIndex) => {
+    todoList.splice(todoIndex, 1);
+    setTodoList([...todoList]);
+  };
+
   return (
     <div>
-      <div>
-        <input
-          placeholder="Write your todo"
-          onChange={(e) => {
-            setTodo(e.target.value);
-          }}
-        />
-        <button
-          onClick={() => {
-            setTodoFunc(todo);
-          }}
-        >
-          Add
-        </button>
+      <div className={style.todoBlock}>
+        <div className={style.textZone}>
+          <textarea
+            placeholder="Write your todo"
+            onChange={(e) => {
+              setTodo(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              setTodoFunc(todo);
+            }}
+          >
+            Add
+          </button>
+        </div>
         <div>
-          {todoList.map((el, index) => {
-            return <li key={index}>{el}</li>;
-          })}
+          <div className={style.noteBlock}>
+            {todoList.map((el, index) => {
+              return (
+                <div key={index} className={style.noteElement}>
+                  <textarea defaultValue={el}></textarea>
+                  <button
+                    onClick={() => {
+                      funcDeleteTodo(index);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
